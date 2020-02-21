7ecf2c7fa9d75b74e2a8be56f366c340c63132ed
melanianf <melania.n@mail.ugm.ac.id>
2020-02-22 06:44:57 +0700

    apifinishkeknya

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('registrasi/name/{name}/email/{email}/password/{password}', 'Auth\Reg
 Route::get('api/login/{username}/pass/{password}', 'myAPIController@siswaLogin');
 Route::get('api/logout/{username}', 'myAPIController@siswaLogout');
 Route::get('api/reguler/{nis}/t/{token}', 'myAPIController@getTabunganReguler');
+Route::get('api/detail/{jenistabungan}/nis/{nis}/t/{token}', 'myAPIController@getDetailTabungan');
 
 Route::group(['midlleware' => 'web'], function() {
 
